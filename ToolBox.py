ebf7cefa29347aef5e126f1ea046ed31efc233e3
maxenlee <maxenlee123@gmail.com>
2024-02-08 22:01:53 -0700

    Update ToolBox.py

--- ToolBox.py
+++ ToolBox.py
@@ -6,8 +6,8 @@ class DataFrameAnalyzer:
 
     def analyze(self):
         df = self._obj  # The DataFrame to analyze
-        
-        # Identify all unique data types in the DataFrame
+
+        # Identify all unique data types in this DataFrame
         all_dtypes = list(df.dtypes.unique())
 
         # Shape of the DataFrame
@@ -15,30 +15,25 @@ class DataFrameAnalyzer:
 
         # Initialize dtype counts series with zeros for all identified dtypes
         dtype_counts = pd.Series(0, index=all_dtypes, name="Dtype Counts")
+        # Update counts with actual values
         dtype_counts.update(df.dtypes.value_counts())
 
-        # Deep memory usage (in bytes)
-        deep_memory_usage = df.memory_usage(deep=True).sum()
-        deep_mem_info = pd.Series([deep_memory_usage], index=["Deep Memory Usage (Bytes)"], name="Memory")
+        # Memory usage (in bytes), deep and shallow
+        total_memory_deep = df.memory_usage(deep=True).sum()
+        total_memory_shallow = df.memory_usage(deep=False).sum()
+        mem_info_deep = pd.Series([total_memory_deep], index=["Deep Memo"], name="Memory")
+        mem_info_shallow = pd.Series([total_memory_shallow], index=["Shallow Mem"], name="Memory")
 
-        # Shallow memory usage (in bytes)
-        shallow_memory_usage = df.memory_usage(deep=False).sum()
-        shallow_mem_info = pd.Series([shallow_memory_usage], index=["Shallow Memory Usage (Bytes)"], name="Memory")
+        # Count of nulls in total
+        total_nulls = pd.Series({'Nulls': df.isnull().sum().sum()})
 
-        # Count of nulls per column
-        null_counts = df.isnull().sum().rename("Null Counts")
+         # Sum of Unique Values Across All Columns
+        total_unique_values = pd.Series({'Unique': sum(df.nunique())})
 
-        # Unique values per column
-        unique_values = df.nunique().rename("Unique Values")
+        # Combining all information into a single DataFrame for analysis
+        summary_stats = pd.concat([shape_info, dtype_counts, mem_info_deep, mem_info_shallow, total_nulls, total_unique_values])
 
-        # Summary statistics for numeric columns
-        numeric_stats = df.describe().transpose()
-
-        # Combining all information
-        summary_df = pd.concat([shape_info, dtype_counts, deep_mem_info, shallow_mem_info, null_counts, unique_values], axis=0)
-        summary_df = pd.concat([summary_df, numeric_stats], axis=0, sort=False)
-
-        return summary_df
+        return summary_stats
 
 # Register the custom accessor on pandas DataFrame with the name "df_kit"
 pd.api.extensions.register_dataframe_accessor("df_kit")(DataFrameAnalyzer)
